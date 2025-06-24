2f96692aa530ad6231e15c4d4f0f1fe6eac84907
Radwa Khaled <radwaabouzed@gmail.com>
2025-06-24 20:47:45 +0300

    up

--- src/App.js
+++ src/App.js
@@ -52,29 +52,3 @@ function App() {
 export default App;
 
 
-
-
-// const invoiceData = {
-//   customer: "عميل نقدي",
-//   date: "24/06/2025",
-//   items: [
-//     { name: "منتج 1", qty: 2, price: 100, discount: 10 },
-//     { name: "منتج 2", qty: 1, price: 200, discount: 0 }
-//   ],
-//   total: 390
-// };
-// function App() {
-//   return (
-//     <div className="App">
-//       <InvoiceHeader />
-//       <ProductTable />
-     
-      
-//       <ActionButtons />
-//          <DownloadInvoicePdf invoiceData={invoiceData} />
-//           <TotalsPanel />
-//           <AccountingEntries />
-//     </div>
-//   );
-// }
-//export default App;
